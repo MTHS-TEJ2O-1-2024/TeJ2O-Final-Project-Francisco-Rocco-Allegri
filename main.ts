503ce0aceb84e54b5d6dc35ef67fefb75b376828
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2025-01-10 11:08:59 -0500

    Added original beta password code and changed readme file

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Francisco Rocco Allegri and Gavin Gallant
+ * Created on: January 2025
+ * This program creates a sequencive password that can only be "unlocked" with a specific order
 */
+let correctPassword: number // - Correct password (the number value total)
+let passwordInput: number // - Number value that is added (subtract or add per button)
+let passwordSequence: number // - Determined amount of times a button is pressed (max 4)
 
-basic.showString('Hello, World!')
+let myStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+myStrip.clear()
+
+//Startup locking code
+
+basic.clearScreen()
+basic.showLeds(`
+. . . . .
+. # . # .
+. . . . .
+# # # # #
+. . . . .
+`)
+correctPassword = 0
+
+//Password sequences
+input.onButtonPressed(Button.A, function () {
+    passwordSequence += 1
+    myStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    myStrip.show()
+    passwordInput += 1
+})
+
+input.onButtonPressed(Button.B, function () {
+    myStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    myStrip.show()
+})
+
+input.onButtonPressed(Button.AB, function () {
+    if (passwordInput = 3) {
+        
+    }
+})
+
+//basic.showIcon(IconNames.Yes)
+//basic.showIcon(IconNames.No)
